a8fd1606b54115e5c55e67408fcbf5f4ce797ce8
James McKay <james.mckay215@yahoo.com>
2026-01-01 14:32:05 -0800

    Cleanup /api/route.ts

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,10 +1,6 @@
-import fs from 'fs';
-import path from 'path';
-import { NextResponse } from 'next/server';
 import { redirect } from 'next/navigation';
 
 export async function GET() {
-  // const documentationFilePath = path.join(process.cwd(), 'public', 'openapi-docs.html');
   // const fileContents = fs.readFileSync(documentationFilePath, 'utf8');
  
   // return new NextResponse(fileContents, {
