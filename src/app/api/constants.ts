93cbcf51fe52fce6a65f103fa78ec7b66b74f944
James McKay <james.mckay215@yahoo.com>
2026-01-22 21:43:04 -0800

    Undo taskfile test

--- src/app/api/constants.ts
+++ src/app/api/constants.ts
@@ -4,5 +4,5 @@ export const boolList = ["true", "false"];
 export const stateList = [
   "AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "DC", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY",
   "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH",
-  "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY", "ZZ"
+  "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"
 ];
